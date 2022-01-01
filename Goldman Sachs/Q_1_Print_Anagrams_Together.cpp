288fecc59d8fa7f6ff5cf83bb08794f7cd7fce08
Sarthak Sharma <44079521+SarthakSharma2001@users.noreply.github.com>
2022-01-01 22:28:09 +0530

    Update Q_1_Print_Anagrams_Together.cpp
    https://practice.geeksforgeeks.org/problems/print-anagrams-together/1/#

--- Goldman Sachs/Q_1_Print_Anagrams_Together.cpp
+++ Goldman Sachs/Q_1_Print_Anagrams_Together.cpp
@@ -1,11 +1,3 @@
-// { Driver Code Starts
-//Initial Template for C++
-#include <bits/stdc++.h>
-#include <unordered_map>
-using namespace std;
-
-
- // } Driver Code Ends
 //User function Template for C++
 
 class Solution{
@@ -25,33 +17,3 @@ class Solution{
         return str;
     }
 };
-
-// { Driver Code Starts.
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        vector<string> string_list(n);
-        for (int i = 0; i < n; ++i)
-            cin>>string_list[i]; 
-        Solution ob;
-        vector<vector<string> > result = ob.Anagrams(string_list);
-        sort(result.begin(),result.end());
-        for (int i = 0; i < result.size(); i++)
-        {
-            for(int j=0; j < result[i].size(); j++)
-            {
-                cout<<result[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
-    }
-
-    return 0;
-}
-  // } Driver Code Ends
