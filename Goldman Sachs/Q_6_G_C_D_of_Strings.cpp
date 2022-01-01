6599a2e193c7238a8e457a730f11ce136c119837
Sarthak Sharma <44079521+SarthakSharma2001@users.noreply.github.com>
2022-01-01 23:12:34 +0530

    Add files via upload
    https://leetcode.com/problems/greatest-common-divisor-of-strings/

---
+++ Goldman Sachs/Q_6_G_C_D_of_Strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        while (str1.length()>0 && str2.length()>0) {
+            if (str1.length() < str2.length()) 
+                swap(str1,str2);
+            int pos;
+            pos = str1.find(str2);
+            if (pos!=0) 
+                return "";
+            str1.erase(pos,str2.length());
+        }
+        if (str1.length()<str2.length()) swap(str1,str2);
+            return str1;        
+    }
+};
