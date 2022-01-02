be06f774ca827845a3435205982cc66a6c7a3f32
Sarthak Sharma <44079521+SarthakSharma2001@users.noreply.github.com>
2022-01-02 22:20:13 +0530

    Add files via upload

---
+++ Goldman Sachs/Q_4_Run_Length_Encoding.cpp
@@ -0,0 +1,17 @@
+string encode(string src)
+{ 
+    string ans; ans.push_back(src[0]); int c=1;
+    for(int i = 1 ; i < src.size() ; i++){
+        if(src[i] == src[i - 1]) 
+            c++;
+        else{
+            ans.push_back('0' + c); 
+            c=1; 
+            ans.push_back(src[i]);
+        }
+    }
+    ans.push_back('0' + c); 
+    return ans;
+  //Your code here 
+}     
+ 
