62fe50308f189283aeaa1aa81fb58d3899486bff
Sarthak Sharma <44079521+SarthakSharma2001@users.noreply.github.com>
2022-01-04 23:56:08 +0530

    Add files via upload

---
+++ Goldman Sachs/Q_12_Squares_in_Chessboard.cpp
@@ -0,0 +1,8 @@
+// Squares in N*N Chessboard
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        // code here
+        return (N * (N + 1) * (2 * N + 1)) / 6;
+    }
+};
