bb47bc117ad1d67a1c01b58cd8250627cc81a0a8
Sarthak Sharma <44079521+SarthakSharma2001@users.noreply.github.com>
2022-01-05 22:58:09 +0530

    Update Q_10_Find_max_10_numbers_in_a_list_having_10M_entries.cpp

--- Goldman Sachs/Q_10_Find_max_10_numbers_in_a_list_having_10M_entries.cpp
+++ Goldman Sachs/Q_10_Find_max_10_numbers_in_a_list_having_10M_entries.cpp
@@ -1,20 +1,39 @@
 // Find max 10 numbers in a list having 10M entries
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
 using namespace std;
-class Solution{
-public:
-    void largest(int arr[],int n,int k)
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // Creating a Priority Queue and Initializing it with first 10 entries.
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int i = 0; i < 10; i++)
     {
-        vector<int> final;
-        priority_queue<int> temp;
-        for(int i = 0 ; i < n ; i++)
-            temp.push(arr[i]);
-        for(int i = 0 ; i < k ; i++)
-        {
-            final.push_back(temp.top());
-            temp.pop();
-        }
-        for(auto it : final)
-            cout<<it<<" ";
+        pq.push(arr[i]);
     }
-};
+
+    // Adding next element and removing the smallest.
+    for (int i = 10; i < n; i++)
+    {
+        pq.push(arr[i]);
+        pq.pop();
+    }
+
+    while (!pq.empty())
+    {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
